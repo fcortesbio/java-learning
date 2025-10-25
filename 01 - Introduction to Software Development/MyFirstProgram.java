82337862fca4386d0f2f77db8b1907d045b8d86b
fcortesbio <fcortesbio@gmail.com>
2025-10-25 06:39:15 -0500

    fix: restructuring code samples. - move code modules outside of nested folers. No longer using Gradle

---
+++ 01 - Introduction to Software Development/MyFirstProgram.java
@@ -0,0 +1,7 @@
+public class MyFirstProgram {
+    public static void main(String[] args){
+        System.out.println("I'm a Programmer!");
+        System.out.println("Let's change the code now!");
+        System.out.println("This is what I do!");
+    }
+}
